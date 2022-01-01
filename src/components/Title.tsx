24beb4ea0974e4316e4fdf4cb63fd7efdfea9666
Andre Burke <aburke.dev@gmail.com>
2022-01-01 16:28:22 -0700

    added more navbar details

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const Title = () => {
   return (
-    <div id="title" className="title-section parallax">
-      <div className="row text-center">
-        <h1 className="title title-color-font">Andre Burke</h1>
-        <h2 className="sub-title title-color-font">Software Engineer</h2>
+    <div id="#" className="title-section parallax">
+      <div className="container">
+        <div className="row text-center">
+          <h1 className="title title-color-font">Andre Burke</h1>
+          <h2 className="sub-title title-color-font">Software Engineer</h2>
+        </div>
       </div>
     </div>
   );
