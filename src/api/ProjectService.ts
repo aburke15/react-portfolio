e6f33b5b92c7bc3b75a5d63c9293d11cd7d4f3c6
Andre Burke <aburke.dev@gmail.com>
2022-01-02 17:55:53 -0700

    added resume component

---
+++ src/api/ProjectService.ts
@@ -0,0 +1,18 @@
+
+
+
+const GetProjects = (): void => {
+
+
+}
+
+const GetProjectDetails = (): void => {
+
+} 
+
+const ProjectService = {
+  GetProjects,
+  GetProjectDetails
+}
+
+export default ProjectService;
---
+++ src/components/Resume.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div id="resume" className="resume-section global-padding">
+      <div className="container text-center">
+        <div className="row">
+          <h2>Resume</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
