8ba000507e55d51a21951a53d49b3c196070ca1d
Andre Burke <aburke.dev@gmail.com>
2022-01-28 19:54:11 -0700

    added docker file

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
-export const env = process.env.NODE_ENV || 'development';
-export const githubToken = process.env.REACT_APP_GITHUB_TOKEN as string;
+export const env = process.env.NODE_ENV || "development";
+export const githubToken = process.env.REACT_APP_GITHUB_PAT as string;
 export const githubUser = process.env.REACT_APP_GITHUB_USER as string;
